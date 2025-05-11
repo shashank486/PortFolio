5f6a1c250d620e1eca426980ff939cf0b4e9d8f3
shashank B <shashankb486@gmail.com>
2025-05-11 20:44:52 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -50,3 +50,20 @@ contactForm.addEventListener('submit', function(event) {
         formStatus.innerHTML = '<p style="color: #f44336;">Network error. Please check your connection and try again.</p>';
     });
 });
+
+// Hamburger
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navMenu = document.getElementById('nav-menu');
+  
+    menuToggle.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+    });
+  
+    document.querySelectorAll('#nav-menu a').forEach(link => {
+      link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+      });
+    });
+  });
+  
